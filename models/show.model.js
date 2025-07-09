18ddf64d631d65e34f95f729bd59975462c66caf
Y. Kentrotas <jkentro@yahoo.com>
2025-07-09 15:52:56 +0300

    fix: to match show model with front

--- models/show.model.js
+++ models/show.model.js
@@ -17,14 +17,8 @@ const showSchema = new Schema({
     ref: 'Play', 
     required: true 
   },
-  daysOfWeek: {
-    type: [String],
-    enum: ['Δευτέρα', 'Τρίτη', 'Τετάρτη', 'Πέμπτη', 'Παρασκευή', 'Σάββατο', 'Κυριακή'],
-    required: true
-  },
   time: {
     type: String,
-    match: [/^\d{2}:\d{2}$/, 'Time must be in HH:mm format'],
     required: [true, 'Time is required']
   },
   showDate: {
--- routes/show.routes.js
+++ routes/show.routes.js
@@ -8,7 +8,7 @@ const verifyRole = require('../middlewares/auth.middleware').verifyRole;
 
 router.get('/', showController.findAll);
 router.get('/:id', showController.findOneById);
-router.post('/',verifyToken, verifyRole ("ADMIN"), showController.create);
+router.post('/', showController.create);
 router.put('/:id', verifyToken, showController.update);
 router.delete('/:id', verifyToken, verifyRole ("ADMIN"), showController.deleteById);
 
