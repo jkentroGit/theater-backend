0b34da69a2374472c1f45253d845d8bba5ab8077
Y. Kentrotas <jkentro@yahoo.com>
2025-07-31 13:06:10 +0300

    fix: sever.js port add

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-// const mongoose = require("mongoose");
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const port = 3000;
 
-
-// require('dotenv').config(); // if you need env variables
 const mongoose = require("mongoose");
-const app = require("./app"); // Import the configured Express app
-const port = 3000;
+const app = require("./app"); 
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI)
 .then (
     () => {
       console.log("Επιτυχημένη σύνδεση με την MongoDB");
+
       
       app.listen(port, ()=>{
         console.log("Επιτυχημένη σύνδεση με server")
